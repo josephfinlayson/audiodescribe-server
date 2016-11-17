05cc1a95cd65a76d959969a517ca7ed90df4ba40
Joseph Finlayson <joseph.finlayson@gmail.com>
2016-11-17 23:40:15 +0100

    initial commit

---
+++ audiodescribe.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, redirect, url_for, send_from_directory, Response
+from utils.RequestHelper import processRequest
+import json
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = './folder'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+emotion_url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
+recognize_url = 'https://api.projectoxford.ai/vision/v1.0/analyze'
+response_headers = dict()
+response_headers['Content-Type'] = 'application/json'
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            result = json.dumps({'error': True, 'message': 'no file')
+            return Response(response=json.dumps(result), content_type='application/json')
+
+
+        file = request.files['file']
+
+        if file:
+            data = file.read()
+
+            params = {'visualFeatures': 'Description,Faces'}
+            result = processRequest(data, params, recognize_url)
+
+            response = Response(response=json.dumps(result), content_type='application/json')
+
+            return response
+
+            # return redirect(url_for('uploaded_file', filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ utils/RequestHelper.py
@@ -0,0 +1,64 @@
+import requests
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+recognize_url = 'https://api.projectoxford.ai/vision/v1.0/analyze'
+_key = '7642486818ac4d34a8e0f0e055d9bcef'  # Here you have to paste your primary key
+json = None
+headers = dict()
+headers['Ocp-Apim-Subscription-Key'] = _key
+headers['Content-Type'] = 'application/octet-stream'
+_maxNumRetries = 1
+
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+
+def processRequest(data, params, url):
+    """
+    Helper function to process the request to Project Oxford
+
+    Parameters:
+    json: Used when processing images from its URL. See API Documentation
+    data: Used when processing image read from disk. See API Documentation
+    headers: Used to pass the key information and the data type request
+    :param url:
+    """
+
+    retries = 0
+    result = None
+
+    while True:
+        response = requests.request('post', url, json=json, data=data, headers=headers, params=params)
+        print response.status_code
+        if response.status_code == 429:
+
+            print("Message: %s" % (response.json()['error']['message']))
+
+            if retries <= _maxNumRetries:
+                time.sleep(1)
+                retries += 1
+                continue
+            else:
+                print('Error: failed after retrying!')
+                break
+
+        elif response.status_code == 200 or response.status_code == 201:
+            print (response.content)
+            if 'content-length' in response.headers and int(response.headers['content-length']) == 0:
+                result = None
+            elif 'content-type' in response.headers and isinstance(response.headers['content-type'], str):
+                if 'application/json' in response.headers['content-type'].lower():
+                    result = response.json() if response.content else None
+                elif 'image' in response.headers['content-type'].lower():
+                    result = response.content
+        else:
+            print("Error code: %d" % (response.status_code))
+            print("Message: %s" % (response.json()))
+
+        break
+
+    return result
