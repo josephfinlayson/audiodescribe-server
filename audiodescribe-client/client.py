ec94054f7a469d430ab0933773e564311db5c7bd
Joseph Finlayson <joseph.finlayson@gmail.com>
2016-11-25 11:25:32 +0100

    do age and gender processing on device

--- audiodescribe-client/client.py
+++ audiodescribe-client/client.py
@@ -6,24 +6,47 @@ from pydub import AudioSegment
 from pydub.playback import play
 import json
 
+#
+# def getDescriptorForImage(image):
+#     payload = {'file': open(image, "rb")}
+#     response = requests.post("http://localhost:5000", files=payload)
+#     print response.content
+#     print response.text
+#     return json.loads(response.content)
+#
+#
+# def sayText(text):
+#     tts = gTTS(text=text, lang='en')
+#     filename = (text[:15] + str(uuid.uuid4().get_hex().upper()[0:6])) + '.mp3'
+#     tts.save(filename)
+#     song = AudioSegment.from_mp3(filename)
+#     play(song)
 
-def getDescriptorForImage(image):
-    payload = {'file': open(image, "rb")}
-    response = requests.post("http://localhost:5000", files=payload)
-    print response.content
-    print response.text
-    return json.loads(response.content)
+
+def reduceFacesIntoGenderAndAge(pv, cv):
+    cv['numberOfMen'] = pv['numberOfMen'] + 1 if cv['gender'] == "Male" else pv['numberOfMen']
+    cv['numberOfWomen'] = pv['numberOfWomen'] + 1 if cv['gender'] == "Female" else pv['numberOfWomen']
+    # map ages into list, get highest and lower of list
+    return cv
 
 
-def sayText(text):
-    tts = gTTS(text=text, lang='en')
-    filename = (text[:15] + str(uuid.uuid4().get_hex().upper()[0:6])) + '.mp3'
-    tts.save(filename)
-    song = AudioSegment.from_mp3(filename)
-    play(song)
 
+def getFaceObject(faceList):
+    ages = map(lambda faceObject: faceObject['age'], faceList)
+    faceObject = reduce(reduceFacesIntoGenderAndAge, faceList, {'numberOfMen': 0, 'numberOfWomen': 0})
 
-descriptor = getDescriptorForImage('sample.jpg')
+    faceObject['ageMax'] = reduce(lambda pv, cv: pv if (pv > cv) else cv, ages)
+    faceObject['ageMin'] = reduce(lambda pv, cv: pv if (pv < cv) else cv, ages)
+    del faceObject['faceRectangle']
+    del faceObject['gender']
+    del faceObject['age']
+
+    return faceObject
+
+def getFaceString(faceObject):
+    return 'it contains ' + str(faceObject['numberOfMen']) + ' men, and ' + str(faceObject['numberOfWomen']) + ' women. The ages ' \
+           'range from ' + str(faceObject['ageMin']) + ' to ' + str(faceObject['ageMax'])
+
 sampleDescriptor = {
     "faces": [{"gender": "Male", "age": 34, "faceRectangle": {"width": 215, "top": 463, "height": 215, "left": 157}},
               {"gender": "Male", "age": 35, "faceRectangle": {"width": 92, "top": 676, "height": 92, "left": 478}},
@@ -40,4 +63,12 @@ sampleDescriptor = {
                  "posing", "restaurant", "large", "dinner", "man", "long", "glasses", "plate", "wine", "pizza",
                  "room"]}, "requestId": "9a667aaa-7234-44cd-89c4-e0657668334c"}
 
-sayText(descriptor['description']['captions'][0]['text'] )
+
+
+processedFaceObject = getFaceObject(sampleDescriptor['faces'])
+faceAndGenderString = getFaceString(processedFaceObject)
+
+# descriptionString = getDescriptorForImage('sample.jpg')
+
+print faceAndGenderString
+# sayText(descriptor['description']['captions'][0]['text'])
