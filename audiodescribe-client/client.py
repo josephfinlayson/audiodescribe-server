8c85c44c4b16eb76086bbddfa79f5f66410381bc
Your Name <you@example.com>
2016-11-26 11:13:34 +0000

    play audio

--- audiodescribe-client/client.py
+++ audiodescribe-client/client.py
@@ -1,20 +1,29 @@
-# import requests
-# from gtts import gTTS
-# import uuid
-# # system dependency on ffmpeg ->
-# from pydub import AudioSegment
-# from pydub.playback import play
-# import json
-#
-# import picamera
+import requests
+from gtts import gTTS
+import uuid
+import time
+import os
+# system dependency on ffmpeg ->
+from pydub import AudioSegment
+from pydub.playback import play
+import json
+from pydub import AudioSegment
+
+
+import picamera
 from gpiozero import Button
 from signal import pause
 
-# camera = picamera.PiCamera()
+API_URL = 'http://51.15.49.230:5000'
+camera = picamera.PiCamera()
 
+def getFileName():
+    return uuid.uuid4().get_hex() + '.jpg'
 
 def recognize():
-    print 'pressed '
+    filename = getFileName()
+    camera.capture(filename)
+    sendRequest(filename, 'recog')
 
 RECOGNIZE_BUTTON_PIN = 18
 recognize_button = Button(RECOGNIZE_BUTTON_PIN)
@@ -22,27 +31,32 @@ recognize_button.when_pressed = recognize
 
 
 def ocr():
-    print 'ocr pressed '
+    filename = getFileName()
+    camera.capture(filename)
+    sendRequest(filename, 'ocr')
 
 OCR_BUTTON_PIN = 23
 ocr_button = Button(OCR_BUTTON_PIN)
 ocr_button.when_pressed = ocr
 
+def sendRequest(filename, type):
+    payload = {'file': open(filename, "rb")}
+    response = requests.post(API_URL, files=payload)
+    content = json.loads(response.content)
+    desc = content['description']
+    print desc
+    sayText(desc)
+
 #
 # def getDescriptorForImage(image):
-#     payload = {'file': open(image, "rb")}
-#     response = requests.post("http://localhost:5000", files=payload)
-#     print response.content
-#     print response.text
-#     return json.loads(response.content)
-#
-#
-# def sayText(text):
-#     tts = gTTS(text=text, lang='en')
-#     filename = (text[:15] + str(uuid.uuid4().get_hex().upper()[0:6])) + '.mp3'
-#     tts.save(filename)
-#     song = AudioSegment.from_mp3(filename)
-#     play(song)
+
+def sayText(text):
+    tts = gTTS(text=text, lang='en')
+    filename = (text[:15] + str(uuid.uuid4().get_hex().upper()[0:6])) + '.mp3'
+    tts.save(filename)
+    one_second_silence = AudioSegment.silent(duration=1000)
+    song = one_second_silence + AudioSegment.from_mp3(filename)
+    play(song)
 
 
 
